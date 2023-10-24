0a8e6cedbfb9242e007ee2a8f0eaf233c9397910
vedantsingh23 <141285386+vedantsingh23@users.noreply.github.com>
2023-10-23 17:56:46 -0700

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,22 @@
+import random
+
+print("""These are the rules of the game:
+      Objective: Guess the word one letter at a time
+      Every correct letter guessed will be revealed
+      After 10 incorrect guesses, the game will end. """)
+
+word_list = ["sunset", "giraffe", "whisper", "zephyrs", "journey", "victory", "explore", "chimney", "blanket", "freedom"]
+
+chosen = random.choice(word_list)
+wordlen = len(chosen)
+
+guessed_word = ["-"] * wordlen
+incorrect_guesses = 0
+
+while incorrect_guesses < 10:
+    guess = input('Guess a single letter: ').lower()
+    if guess in chosen:
+        for i in range wordlen:
+            if chosen_word[i] == guess:
+                guessed_word[i] = guess
+                
